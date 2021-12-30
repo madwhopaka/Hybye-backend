d6723d0df795eb91246985878e6f4662466da266
madhu paka <madhupaka11@gmail.com>
2021-12-30 21:27:08 +0530

    Edited generateRoom code

--- routes/create.js
+++ routes/create.js
@@ -8,8 +8,9 @@ function generateRoomCode(length) {
   
     let result = '';
     const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
+    for ( let i = 0; i < 10; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        result+= Math.floor(Math.random()*100) ; 
     }
 
     return result;
